6e180b5e98d406047d14a81370b864c689dd7b48
Tichomir Mitkov <MitkovTihomir@gmail.com>
2018-10-27 19:37:28 +0300

    Beginning organising the project

--- Init.pm
+++ Init.pm
@@ -1,9 +1,10 @@
-#!/usr/bin/perl -w
-
-#TODO install hooks for window size change
-#FIXME When the -y option is set to the texteditor widget, the statusBar (Label) disappears. Fix that
+package Init;
 
 use strict;
+
+use lib '.';
+use Model::Window;
+
 use Curses::UI;
 use Curses::UI::Common;
 use Data::Dumper;
@@ -11,12 +12,15 @@ use Switch;
 
 my $gMode = "NULL";
 
+my $cui = new Curses::UI( -color_support => 1 );
+
+
 sub editDummy{
     my $textEditor = shift;
 
     if ($gMode eq "normal")
     {
-	#$textEditor->undo();
+        #$textEditor->undo();
     }
 }
 
@@ -34,102 +38,103 @@ sub setEditorWidget{
     sub cusAppend { ${$textEditor}->cursor_right; &setModeInput(); }
 
     sub cusAppendLine { 
-	#DONE go to input mode here
-	&setModeInput();
+        #DONE go to input mode here
+        &setModeInput();
 
-	${$textEditor}->cursor_to_scrlineend();
-	${$textEditor}->newline();
+        ${$textEditor}->cursor_to_scrlineend();
+        ${$textEditor}->newline();
     }
 
     sub cusPrependLine { 
-	#DONE go to input mode here
-	&setModeInput();
+        #DONE go to input mode here
+        &setModeInput();
 
-	${$textEditor}->cursor_to_scrlinestart();
-	${$textEditor}->newline();
-	${$textEditor}->cursor_up();
+        ${$textEditor}->cursor_to_scrlinestart();
+        ${$textEditor}->newline();
+        ${$textEditor}->cursor_up();
     }
 
     sub statusEntryCance{
-	${$statusEntry}->{-text} = "";
-	${$statusBar}->{-text} = "";
-	${$statusBar}->bold(1);
+        ${$statusEntry}->{-text} = "";
+        ${$statusBar}->{-text} = "";
+        ${$statusBar}->bold(1);
 
-	${$textEditor}->focus();
-	$gMode = "normal";
+        ${$textEditor}->focus();
+        $gMode = "normal";
     }
 
     sub statusEntryExecute{
-	#TODO do something with statusEntry's -text
-	my $command = ${$statusEntry}->{-text};
+        #TODO do something with statusEntry's -text
+        my $command = ${$statusEntry}->{-text};
 
-	switch ($command)
-	{
-	    case "q" { &exit_dialog() }
-	}
+        switch ($command)
+        {
+            case "q" { exit_dialog() }
+            case "w" { }
+            case "open" { open_file() }
+        }
 
-	${$statusEntry}->{-text} = "";
-	${$statusBar}->{-text} = "";
-	${$statusBar}->bold(1);
+        ${$statusEntry}->{-text} = "";
+        ${$statusBar}->{-text} = "";
+        ${$statusBar}->bold(1);
 
-	${$textEditor}->focus();
-	$gMode = "normal";
+        ${$textEditor}->focus();
+        $gMode = "normal";
     }
 }
 
 
-my $cui = new Curses::UI( -color_support => 1 );
 my $dump;
 
 my @menu = (
-  { -label => 'File', 
-    -submenu => [
-   { -label => 'Exit      ^Q', -value => \&exit_dialog  }
-		]
-   },
+    { -label => 'File', 
+        -submenu => [
+            { -label => 'Exit      ^Q', -value => \&exit_dialog  }
+        ]
+    },
 );
 
 sub exit_dialog()
 {
     my $return = $cui->dialog(
-	    -message   => "Do you really want to quit?",
-	    -title     => "Are you sure???", 
-	    -buttons   => ['yes', 'no'],
+        -message   => "Do you really want to quit?",
+        -title     => "Are you sure???", 
+        -buttons   => ['yes', 'no'],
 
     );
 
     if ($return)
     {
-	exit(0);
+        exit(0);
     }
 }
 
 my $win1 = $cui->add(
-		     'win1', 'Window',
-		     -y    => 0,
-	     );
+    'win1', 'Window',
+    -y    => 0,
+);
 
 my $texteditor = $win1->add("text", "TextEditor",
-			-border => 0,
-			-bfg  => 'red',
-			-undolevels => 4096,
-			#-y => 1,
-			-onchange => \&editDummy,
-			-wrapping => 1,
-			-height => $win1->height-1,
-			-text => "Here is some text\n"
-				. "And some more");
+    -border => 0,
+    -bfg  => 'red',
+    -undolevels => 4096,
+    #-y => 1,
+    -onchange => \&editDummy,
+    -wrapping => 1,
+    -height => $win1->height-1,
+    -text => "Here is some text\n"
+    . "And some more");
 
 my $statusBar = $win1->add('statusbar', "Label",
-			-y => $texteditor->height,
-			-width => -1,
-			-text => "");
-    
+    -y => $texteditor->height,
+    -width => -1,
+    -text => "");
+
 my $statusEntry = $win1->add('statusEntry', "TextEntry",
-			-y => $texteditor->height,
-			-x => 1,
-			-width => -1,
-			-text => "");
+    -y => $texteditor->height,
+    -x => 1,
+    -width => -1,
+    -text => "");
 
 sub setModeInput
 {
@@ -160,9 +165,9 @@ sub setModeInput
 }
 
 #!TASK add keybinding:
-    #clear binding in setModeInput()
-    #add the key in @normalModeKeys
-    #set binding in setModeNormal()
+#clear binding in setModeInput()
+#add the key in @normalModeKeys
+#set binding in setModeNormal()
 
 #!--------------------------------------------------------------
 #!--------------------------------------------------------------
@@ -180,18 +185,18 @@ sub setModeInput
 #DONE bind '^' and '$'
 #DONE bind 'x'
 #DONE try to add TextEntry on top of the Label, or somehow swap between them
-    #the statusLabel is never deleted. When in command mode its width shrinks to 1 character and displays only ":"
-    #DONE the statusLabel should go between width 1 and width -1
-	#setting width -1 solved the problem. It seems that curses allows for two widgets to occupy the same position
-    #DONE ESC key inside statusEntry should bring focus to the TextEditor widget
+#the statusLabel is never deleted. When in command mode its width shrinks to 1 character and displays only ":"
+#DONE the statusLabel should go between width 1 and width -1
+#setting width -1 solved the problem. It seems that curses allows for two widgets to occupy the same position
+#DONE ESC key inside statusEntry should bring focus to the TextEditor widget
 #DONE install hook for RETURN key in the statusEntry
 #NNOTE mode-command may be redudant because the focus is on different widget
 #TODO make all widget-referencing functions closures
 #DEBUGGED When hitting RETURN inside StatusEntry the focus goes to the TextEditor, but the keys seem to have lost their 
-    #event handlers. This is because in command mode the hooks are disabled. Without RETURN hook, the mode is not set
-    #properly. 
+#event handlers. This is because in command mode the hooks are disabled. Without RETURN hook, the mode is not set
+#properly. 
 #DONE when hitting enter inside statusEntry, going back to command mode doesn't work. This is because we don't have a 
-    #hook for the enter key. So now the program thinks it is still in command mode, and refuses to change it
+#hook for the enter key. So now the program thinks it is still in command mode, and refuses to change it
 #FIXED 'O' doesn't work properly when the cursor is at the first line of the buffer.
 #TODO the ESC key seems to wait for another key or something. Make it 'non-greedy' (e.g. not combine with other characters)
 #TODO execute commands from statusEntry
@@ -200,7 +205,7 @@ sub setModeInput
 #!CONSIDER assign multi-key sequences
 #!CONSIDER the undo history should work as in vim - a single 'u' stroke should delete everything from the last 'i' edit
 #!CONSIDER bind 'v' and 'V'
-    #This will require a lot of work	file://ncurses - Highlighting and Selecting text with Python curses - Stack Overflow.html
+#This will require a lot of work	file://ncurses - Highlighting and Selecting text with Python curses - Stack Overflow.html
 #!CONSIDER switch to newer version of curses that supports UTF-8 (for example ncursesw)
 
 my @normalModeKeys = qw(h i j k l u o O a : D x ^ $);
@@ -210,7 +215,7 @@ sub setModeNormal
 {
     if ($gMode eq "normal")
     {
-	return;
+        return;
     }
 
     $gMode = "normal";
@@ -227,13 +232,13 @@ sub setModeNormal
 
     for (my $k=1; $k<=255; $k++)
     {   
-	if(ord $key == $k)
-	{   
-	    ($ak, $key) = each @normalModeKeys;
-	    next;
-	}
+        if(ord $key == $k)
+        {   
+            ($ak, $key) = each @normalModeKeys;
+            next;
+        }
 
-	$texteditor->set_binding("edit-dummy", chr $k);
+        $texteditor->set_binding("edit-dummy", chr $k);
     }
 
     $texteditor->set_binding("my-cursor-left", "h");
@@ -259,7 +264,7 @@ sub setModeNormal
 sub setModeCommand{
     if ($gMode eq "command")
     {
-	return;
+        return;
     }
 
     $gMode = "command";
@@ -278,40 +283,42 @@ sub setModeCommand{
 }
 
 
-setEditorWidget(\$texteditor, \$statusBar, \$statusEntry);
+sub run {
+    setEditorWidget(\$texteditor, \$statusBar, \$statusEntry);
 
-#...register routines
-$texteditor->set_routine('my-cursor-left', \&cusCursorLeft);
-$texteditor->set_routine('my-cursor-right', \&cusCursorRight);
-$texteditor->set_routine('my-cursor-up', \&cusCursorUp);
-$texteditor->set_routine('my-cursor-down', \&cusCursorDown);
-$texteditor->set_routine('my-undo', \&cusUndo);
-$texteditor->set_routine('my-append-line', \&cusAppendLine);
-$texteditor->set_routine('my-prepend-line', \&cusPrependLine);
-$texteditor->set_routine('my-append', \&cusAppend);
+    #...register routines
+    $texteditor->set_routine('my-cursor-left', \&cusCursorLeft);
+    $texteditor->set_routine('my-cursor-right', \&cusCursorRight);
+    $texteditor->set_routine('my-cursor-up', \&cusCursorUp);
+    $texteditor->set_routine('my-cursor-down', \&cusCursorDown);
+    $texteditor->set_routine('my-undo', \&cusUndo);
+    $texteditor->set_routine('my-append-line', \&cusAppendLine);
+    $texteditor->set_routine('my-prepend-line', \&cusPrependLine);
+    $texteditor->set_routine('my-append', \&cusAppend);
 
-$texteditor->set_routine('mode-input', \&setModeInput);
-$texteditor->set_routine('mode-normal', \&setModeNormal);
-$texteditor->set_routine('mode-command', \&setModeCommand);
-$texteditor->set_routine('edit-dummy', \&editDummy);
+    $texteditor->set_routine('mode-input', \&setModeInput);
+    $texteditor->set_routine('mode-normal', \&setModeNormal);
+    $texteditor->set_routine('mode-command', \&setModeCommand);
+    $texteditor->set_routine('edit-dummy', \&editDummy);
 
-$statusEntry->set_routine('my-cancel-command', \&statusEntryCance);
-$statusEntry->set_routine('my-execute-command', \&statusEntryExecute);
+    $statusEntry->set_routine('my-cancel-command', \&statusEntryCance);
+    $statusEntry->set_routine('my-execute-command', \&statusEntryExecute);
 
-#obviously this should not be changed
-$statusEntry->set_binding("my-cancel-command", CUI_ESCAPE);
-$statusEntry->set_binding("my-execute-command", Curses::KEY_ENTER());
+    #obviously this should not be changed
+    $statusEntry->set_binding("my-cancel-command", CUI_ESCAPE);
+    $statusEntry->set_binding("my-execute-command", Curses::KEY_ENTER());
 
-setModeNormal();
+    setModeNormal();
 
-$cui->set_binding( \&exit_dialog , "\cX");
+    $cui->set_binding( \&exit_dialog , "\cX");
 
-#$texteditor->focus();
-#$texteditor->readonly(1);
+    #$texteditor->focus();
+    #$texteditor->readonly(1);
 
-$statusBar->bold(1);
+    $statusBar->bold(1);
 
-$cui->mainloop();
+    $cui->mainloop();
+}
 
 __DATA__
 
@@ -323,7 +330,7 @@ $texteditor->set_binding(sub {$texteditor->cursor_left()}, "h");
 
 $cui->set_binding(sub {$menu->focus(); $win1->refresh($win1)}, CUI_ESCAPE );
 my $menu = $cui->add(
-	'menu','Menubar', 
-	-menu => \@menu,
-	-fg  => "blue",
+'menu','Menubar', 
+-menu => \@menu,
+-fg  => "blue",
 );
